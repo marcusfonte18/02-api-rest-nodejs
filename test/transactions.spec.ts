4f4de7e7bb122af1d6a5754d54c4c238c9c95720
Marcus Vinicius <marcus.fontel@rededor.com.br>
2023-12-03 15:40:50 -0300

    03-testando-listagem-transacao

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,4 +1,4 @@
-import { afterAll, beforeAll, describe, it, test } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it, test } from 'vitest'
 import { app } from '../src/app'
 import request from 'supertest'
 
@@ -20,5 +20,39 @@ describe('Transactions Route', () => {
         type: 'credit',
       })
       .expect(201)
+  })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionsResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transactions',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionsResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    // 2 jeitos de resolver o problema de esperar o ID mas ele é dinamico
+
+    // desse jeito
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New Transactions',
+        amount: 5000,
+      }),
+    ])
+
+    // Ou desse jeito
+    // expect(listTransactionsResponse.body.transactions).toEqual([
+    //   {
+    //     id: expect.any(String),
+    //   },
+    // ])
   })
 })
